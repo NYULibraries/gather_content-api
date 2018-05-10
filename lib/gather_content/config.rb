30c3fae7ed1af824d8f7a052861e87fcf088be12
Myles Eftos <myles@madpilot.com.au>
2018-05-10 14:40:55 +1000

    Autoloading DSL and config files

---
+++ lib/gather_content/config.rb
@@ -0,0 +1,7 @@
+module GatherContent
+  module Config
+    autoload :Builder, 'gather_content/config/builder'
+    autoload :Tab, 'gather_content/config/tab'
+    autoload :Elements, 'gather_content/config/elements'
+  end
+end
---
+++ lib/gather_content/dsl.rb
@@ -0,0 +1,14 @@
+module GatherContent
+  module DSL
+    autoload :Base, 'gather_content/dsl/base'
+    autoload :ChoiceCheckbox, 'gather_content/dsl/choice_checkbox'
+    autoload :ChoiceRadio, 'gather_content/dsl/choice_radio'
+    autoload :Config, 'gather_content/dsl/config'
+    autoload :Files, 'gather_content/dsl/files'
+    autoload :Option, 'gather_content/dsl/option'
+    autoload :OtherOption, 'gather_content/dsl/other_option'
+    autoload :Section, 'gather_content/dsl/section'
+    autoload :Tab, 'gather_content/dsl/tab'
+    autoload :Text, 'gather_content/dsl/text'
+  end
+end
